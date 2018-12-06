fbf53e11db4b551db9693c389da44bda1716ed9c
jfhawkin <jfhawkin@gmail.com>
2018-12-06 15:18:03 -0500

    Initial addition of LM tests. Untested.

--- sim_boxcox.py
+++ sim_boxcox.py
@@ -3,10 +3,16 @@ import pandas as pd
 import pysal as ps
 import matplotlib.pyplot as plt
 
-NSIMS = 100
+NSIMS = 1
 NOBS = 2000
+# Indicates whether to estimate Box-Cox transformation
+BC = 1
 
-x_names = ["hhveh", "hhsize", "hhinc", "sex", "taz_avg_price", "autotrip", "activetrip"]
+# x_names = ["lic", "hhsize", "hhinc", "sex", "taz_avg_price", "autotrip", "activetrip", "t_long", "t_local"]
+# x_names = ["lic", "hhsize", "hhinc", "sex", , "autotrip", "activetrip", "transpass"]
+# x_names = ["lic", "hhsize", "hhinc", "sex", "autotrip", "activetrip", "t_GO", "t_local", "t_PRESTO"]
+# x_names = ["lic", "hhsize", "hhinc", "sex", "autotrip", "activetrip", "t_long", "t_local"]
+x_names = ["lic", "hhsize", "hhinc", "sex", "activetrip", "t_long", "t_local"]
 
 beta_array = np.zeros((NSIMS, len(x_names) + 3))
 std_err_array = np.zeros((NSIMS, len(x_names) + 3))
@@ -24,42 +30,55 @@ for i in range(1, NSIMS+1, 1):
     y_name = "workdist"
     y = db[y_name].values
     y = y[:, np.newaxis]
-    # Convert distances in meters to 10s of km
-    y = y / 10**4
+    # Convert distances in meters to km
+    y = y / 10 ** 3
     # Update average zone price variable to scale and remove nan (replace with average for region)
     db['taz_avg_price'] = db['taz_avg_price'] / 10**4
     db['taz_avg_price'].fillna(73.98787585, inplace=True)
+
+    # PRESTO and GO are similar, so combine
+    db['t_long'] = ((db['transpass'] == 1) | (db['transpass'] == 2) | (db['transpass'] == 6)).astype(int)
+    db['t_local'] = ((db['transpass'] == 3) | (db['transpass'] == 5)).astype(int)
+
     x = db[x_names].values
     ww = np.loadtxt('D:/PhD/Thesis/estimation/Spatial_SEM_Model/data/weight{0}.csv'.format(i), delimiter=',')
     ww = ((ww < 1000.0) & (ww > 0.0))*1
     w = ps.weights.util.full2W(ww)
-    mllag = ps.spreg.ML_Lag_BC(y,x,w,name_y=y_name,name_x=x_names,name_ds=ds_name)
-    beta_array[i-1, :] = np.squeeze(mllag.betas)
-    std_err_array[i-1, :] = np.squeeze(mllag.std_err)
-    # print(mllag.summary)
+    if BC == 1:
+        # Perform analysis on distance in meters because gives more values > 1.0
+        mllag = ps.spreg.ML_Lag_BC(y, x, w, name_y=y_name, name_x=x_names, name_ds=ds_name, LM=True)
+    else:
+        mllag = ps.spreg.ML_Lag(y, x, w, name_y=y_name, name_x=x_names, name_ds=ds_name)
 
-# Print the lists of beta parameters and standard errors for all simulations
-print(np.mean(beta_array, axis=0))
-print(np.mean(std_err_array, axis=0))
+    if NSIMS == 100:
+        beta_array[i-1, :] = np.squeeze(mllag.betas)
+        std_err_array[i-1, :] = np.squeeze(mllag.std_err)
+    else:
+        print(mllag.summary)
 
-beta_means, beta_std = tuple(np.mean(beta_array, axis=0)), tuple(np.std(beta_array, axis=0))
-std_err_means, std_err_std = tuple(np.mean(std_err_array, axis=0)), tuple(np.std(std_err_array, axis=0))
+if NSIMS == 100:
+    # Print the lists of beta parameters and standard errors for all simulations
+    print(np.mean(beta_array, axis=0))
+    print(np.mean(std_err_array, axis=0))
 
-ind = np.arange(len(beta_means))  # the x locations for the groups
-width = 0.25  # the width of the bars
+    beta_means, beta_std = tuple(np.mean(beta_array, axis=0)), tuple(np.std(beta_array, axis=0))
+    std_err_means, std_err_std = tuple(np.mean(std_err_array, axis=0)), tuple(np.std(std_err_array, axis=0))
 
-fig, ax = plt.subplots()
-rects1 = ax.bar(ind - width/2, beta_means, width, yerr=beta_std,
-                color='SkyBlue', label='Betas')
-rects2 = ax.bar(ind + width/2, std_err_means, width, yerr=std_err_std,
-                color='IndianRed', label='Standard Errors')
+    ind = np.arange(len(beta_means))  # the x locations for the groups
+    width = 0.25  # the width of the bars
 
-# Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Value')
-ax.set_title('Bootstrapped Mean and Standard Errors for 100 Samples')
-ax.set_xticks(ind)
-ax.set_xticklabels(("CONSTANT","HHVEH", "HHSIZE", "HHINC", "SEX", "AVG PRICE", "AUTOTRIPS", "ACTIVETRIPS", "RHO", "LAMBDA"))
-ax.legend()
-ax.xticks(rotation=45)
+    fig, ax = plt.subplots(figsize=(10, 10))
+    rects1 = ax.bar(ind - width/3, beta_means, width, yerr=beta_std,
+                    color='SkyBlue', label='Betas')
+    rects2 = ax.bar(ind + width/3, std_err_means, width, yerr=std_err_std,
+                    color='IndianRed', label='Standard Errors')
 
-plt.savefig('boxcox.png')
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Value')
+    ax.set_title('Bootstrapped Mean and Standard Errors for 100 Samples')
+    ax.set_xticks(ind)
+    ax.set_xticklabels(("CONSTANT", "HHVEH", "HHSIZE", "HHINC", "SEX", "AVG PRICE", "AUTOTRIPS", "ACTIVETRIPS", "RHO", "LAMBDA"))
+    ax.legend()
+    plt.xticks(rotation=45)
+
+    plt.savefig('boxcox.png')
